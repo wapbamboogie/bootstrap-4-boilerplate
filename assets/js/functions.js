acd42d9201ba780787daed0c8ad22e5a8faffa13
daut-molahasanov <42000588+daut-molahasanov@users.noreply.github.com>
2018-08-24 13:07:36 +0300

    Up

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,19 @@
 $( document ).ready(function() {
 
-  // Get started!
+  $('body').scrollspy({target: ".navbar", offset: $("#scrollspy").height()});
+  $("#scrollspy ul li a").on('click', function(e) {
+
+    e.preventDefault();
+ 
+    var hash = this.hash;
+    
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+
+        window.location.hash = hash;
+      });
+ 
+ });
 
 });
